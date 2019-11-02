049b4e5e72554cdcbc59b4fab659e27413fdd6a2
John Trecker <jttrecker@gmail.com>
2019-11-02 00:08:16 -0700

    clarify db connection values

--- server/syntopicon/configurations/utils.py
+++ server/syntopicon/configurations/utils.py
@@ -5,5 +5,5 @@ def parse_pgql_string_from_env(db):
     user = environ.get(db + '_USER')
     password = environ.get(db + '_PASSWORD')
     host = environ.get(db + '_HOST')
-    table = environ.get(db + '_DATABASE')
-    return 'postgresql+psycopg2://%s:%s@%s/%s' % (user, password, host, table)
+    database = environ.get(db + '_DATABASE')
+    return 'postgresql+psycopg2://%s:%s@%s/%s' % (user, password, host, database)
