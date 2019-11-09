19f185c8ca05616c7cae4d35d2a2b1cb5f51c8e8
John Trecker <jttrecker@gmail.com>
2019-11-08 21:28:04 -0800

    refactor to functional component with hooks

--- client/src/components/ConceptTree/ConceptTree.jsx
+++ client/src/components/ConceptTree/ConceptTree.jsx
@@ -1,96 +1,64 @@
 import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import Tree from 'react-d3-tree';
-import './ConceptTree.scss'
 import NodeLabel from 'components/NodeLabel'
+import useWindowDimensions from 'hooks/useWindowDimensions'
 
+import './ConceptTree.scss'
 
-class ConceptTree extends React.Component {
-  constructor(props){
-    super()
-    this.state = {
-      translate: {},
-      dimensions: { width: undefined, height: undefined},
-      selected: undefined
-    }
-  }
+const ConceptTree = (props) => {
+  const { width, height } = useWindowDimensions()
+  const _pathwidth = 250
+  const _baseY = height / 4
+  const _baseX = width / 2 - _pathwidth
 
-  componentDidMount() {
-    const dimensions = this.treeContainer.getBoundingClientRect()
-    this.setState({
-      translate: {
-        x: dimensions.width / 4,
-        y: dimensions.height / 3
-      },
-      dimensions: {
-        width: dimensions.width,
-        height: dimensions.height
-      }
-    })
-  }
+  const [selected, setSelected] = useState()
+  const [translate, setTranslate] = useState({ x: _baseX, y: _baseY })
 
-  // TODO:
-  componentWillUnmount(){
-    // destroy d3 root node
-    // clear intervals
-    // remove event listeners
-  }
+  useEffect(() => {
+    if (!selected) setTranslate({ x: _baseX, y: _baseY })
+  }, [props.data.name])
 
-  componentDidUpdate(prevProps){
-    if (prevProps.data.name !== this.props.data.name) {
-      const { width, height } = this.state.dimensions      
-      this.setState({translate: {x: width / 4, y: height / 3}})
-    }
-  }
-
-  handleClick = (nodeData, evt) => {
+  const handleClick = (nodeData, evt) => {
     const { _collapsed, depth, meta, children } = nodeData
-    const { width, height } = this.state.dimensions
     if (depth === 1) {
-      if (_collapsed) this.setState({translate: {x: width / 4, y: height / 3}})
-      if (!_collapsed) this.setState({translate: {x: width / 8, y: height / 3}})
+      if (_collapsed) setTranslate({ x: _baseX, y: _baseY})
+      if (!_collapsed) setTranslate({ x: _baseX - _pathwidth, y: _baseY})
     }
     if (depth === 2) {
       if (!meta.subtopic_id) return
-      this.setState({selected: meta.subtopic_id})
+      setSelected(meta.subtopic_id)
     }
   }
 
-  render() {
-    const { children } = this.props.data
-    const { selected, translate } = this.state
-    const nodeSize = {
-      x: 250,
-      y: children.length > 3 ? 55 : 150
-    }
-    return (
-      <div className='tree-container' ref={tc => (this.treeContainer = tc)}>
-        <Tree
-          className='svg-tree'
-          data={this.props.data}
-          nodeSize={nodeSize}
-          textLayout={{textAnchor: "start", x: 25, y: 5 }}
-          translate={translate}
-          nodeLabelComponent={NodeLabel}
-          separation={{siblings: 1, nonSiblings: 1}}
-          onClick={this.handleClick}
-          style={{
-            transform: 'translate(50%)'
-          }}
-          allowForeignObjects
-          shouldCollapseNeighborNodes
-          useCollapseData
-        />
-        {selected && <Redirect
+  const nodeSize = { x: _pathwidth, y: props.data.children.length > 3 ? 55 : 150 }
+
+  return (
+    <div className='tree-container'>
+      <p style={{ border: '1px solid red;', position: 'absolute', top: '50%', left: 0}}>{width} ~ {height}</p>
+      <Tree
+        className='svg-tree'
+        data={props.data}
+        nodeLabelComponent={NodeLabel}
+        nodeSize={nodeSize}
+        onClick={handleClick}
+        separation={{siblings: 1, nonSiblings: 1}}
+        textLayout={{textAnchor: "start", x: 25, y: 5 }}
+        translate={translate}
+        allowForeignObjects
+        shouldCollapseNeighborNodes
+        useCollapseData
+        // style={{transform: 'translate(50%)'}}
+      />
+      {selected && <Redirect
         to={{
           pathname: "/references",
           search: `?sub=${selected}`,
           state: { id: selected }
         }}
-      />}
-      </div>
-    )
-  }
+    />}
+    </div>
+  )
 }
 
 export default ConceptTree;
