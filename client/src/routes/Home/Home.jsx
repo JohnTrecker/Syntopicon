0f019b4863e631a6a07c8ce174ccf8d8ab3c458a
John Trecker <jttrecker@gmail.com>
2019-11-14 01:47:35 -0800

    fix variable Nav width bug

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -5,7 +5,7 @@ import './Home.scss'
 
 function Home() {
   return (
-    <div className="home-sections">
+    <div className="home--container">
       <Section type="section-topics" title="Peruse" href="/topics" />
       <Section type="section-search" title="Search" href="/search" />
       <Section type="section-follow" title="Follow" href="/account" />
